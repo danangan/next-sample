a4e7c614ea5e7f6e4f55693be1223d06f7640928
Danang Nur <danang.arbansa@personio.de>
2021-08-19 23:48:54 +0200

    Index page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import {
+  Content,
+  Header,
+  HeaderName,
+  HeaderNavigation,
+  HeaderMenuItem
+} from 'carbon-components-react/lib/components/UIShell';
+import Link from 'next/link';
+import '../styles/externals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <Header aria-label="IBM Platform Name">
+      <HeaderName href="#" prefix="">
+        Danang Nur
+      </HeaderName>
+      <HeaderNavigation>
+        <Link href="/"><HeaderMenuItem>Home</HeaderMenuItem></Link>
+        <Link href="/blog"><HeaderMenuItem>Blog</HeaderMenuItem></Link>
+      </HeaderNavigation>
+    </Header>
+    <Content>
+      <Component {...pageProps} />
+    </Content>
+  </div>
 }
 export default MyApp
